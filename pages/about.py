ee62babfcac0c57bae18cc2ad920e597f0bc789f
Dillon Ragar <dillonragar@gmail.com>
2025-03-31 20:12:45 -0700

    Add description

--- pages/about.py
+++ pages/about.py
@@ -15,21 +15,40 @@ import numpy as np
 
 dash.register_page(__name__)
 
-
 layout = html.Div(
     [
         html.Div(
             [
-                dcc.Markdown(
-                    """
-
-            ### Coming Soon.
-
-
-            """
+                html.Div(
+                    [
+                        # html.H1("Dangermond Preserve Model Ouput Explorer"),
+                        html.Br(),
+                        html.H2("Overview"),
+                        html.P(
+                            "This dashboard serves model output, historic water balance, and station data from instrumented "
+                            "groundwater wells in the Dangermond Preserve. The primary use is to visualize model results from CFE, "
+                            "a conceptual hydrologic model that offers a Basic Model Interface (BMI) compatible version of the National Water Model. "
+                            #
+                            "The dashboard serves as an entry point to the data, visualizing monthly aggregations of water balance variables, observations, "
+                            "and forcing variables. The full data collection is available at _addlink_, which includes detail and descriptions of the "
+                            "output variables, model parameters, calibration steps, and example code for reading output files."
+                        ),
+                        html.Br(),
+                        html.P(
+                            [
+                                "The source code to run this app is available on github: ",
+                                html.A(
+                                    "Dangermond Web App Repo",
+                                    href="https://your-link-here",
+                                    target="_blank",
+                                ),
+                                ". The repo also contains the code used to generate the monthly aggregations, calculated volumes, and summary statistics.",
+                            ]
+                        ),
+                    ],
+                    className="readable-text",
                 ),
             ],
-            style={"padding": "1rem 1rem 1rem 1rem"},
         ),
     ]
 )
